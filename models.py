911b13efd3fb32d3dce15b7b29e12450ce1f410c
krissy <kmujakic@gmail.com>
2023-10-21 17:13:16 +0200

    employees and team models if working with real data

--- models.py
+++ models.py
@@ -6,29 +6,43 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
+class Employee(db.Model):
+    """Employee model"""
+
+    __tablename__ = 'employees'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    submissions = db.relationship('Submission', backref='employee')
+
+
 class Submission(db.Model):
     """Submission model"""
 
     __tablename__ = 'submissions'
 
-    id = db.Column(db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
-    
-    submission_start = db.Column(db.String,
-                                 nullable=False)
-    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    submission_start = db.Column(db.String, nullable=False)
     prompt_1 = db.Column(db.String)
     prompt_1_resp = db.Column(db.String)
     prompt_2 = db.Column(db.String)
     prompt_2_resp = db.Column(db.String)
     summary = db.Column(db.String)
-    
     created_at = db.Column(
         db.DateTime,
         nullable=False,
         default=datetime.utcnow(),
     )
+    employee_id = db.Column(db.Integer, db.ForeignKey(
+        'employees.id'), nullable=False)
 
-    # @classmethod
-    # def build_submission(cls):
+
+class Team(db.Model):
+    """Team model"""
+
+    __tablename__ = 'teams'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    employees = db.relationship('Employee', backref='team')
