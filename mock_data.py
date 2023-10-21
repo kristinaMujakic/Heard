834169136554ca5dc57bf7bec7fa79949aa03f2d
krissy <kmujakic@gmail.com>
2023-10-21 17:10:55 +0200

    adding data route to show AI submissions

---
+++ mock_data.py
@@ -0,0 +1,55 @@
+mock_employees = [
+    {
+        'id': 1,
+        'name': 'Employee 1',
+        'team_id': 1,
+    },
+    {
+        'id': 2,
+        'name': 'Employee 2',
+        'team_id': 1,
+    },
+    {
+        'id': 3,
+        'name': 'Employee 3',
+        'team_id': 2,
+    },
+    {
+        'id': 4,
+        'name': 'Employee 4',
+        'team_id': 2,
+    },
+    {
+        'id': 5,
+        'name': 'Employee 5',
+        'team_id': 3,
+    },
+]
+
+mock_submissions = [
+    {
+        'id': 1,
+        'employee_id': 1,
+        'message': 'I often face communication issues with my team, which affects my work. It would be better if my manager helps improve team collaboration.',
+    },
+    {
+        'id': 2,
+        'employee_id': 2,
+        'message': 'I need more training in our new software tools to become more efficient at work.',
+    },
+    {
+        'id': 3,
+        'employee_id': 3,
+        'message': 'I find it challenging to meet tight deadlines. It would help if we have better time management strategies within the team.',
+    },
+    {
+        'id': 4,
+        'employee_id': 4,
+        'message': 'I would be more productive if I had a quieter workspace.',
+    },
+    {
+        'id': 5,
+        'employee_id': 5,
+        'message': 'Improved feedback from my manager would help me grow in my role.',
+    },
+]
